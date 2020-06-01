b4b7399dca6ea77397c0e7a92c4f14b1bdecf7c6
zhenghetian <zhenghetian@finupgroup.com>
2020-06-01 14:14:05 +0800

    排序

--- src/main/java/hashmap/HashMapTest.java
+++ src/main/java/hashmap/HashMapTest.java
@@ -12,6 +12,7 @@ public class HashMapTest {
 
     public static void main(String[] args) {
         HashMap hashMap = new HashMap();
+        hashMap.put("a",2);
 
     }
 }
---
+++ src/main/java/thradlocal/ThreadLocalTest.java
@@ -0,0 +1,23 @@
+package thradlocal;
+
+import com.sun.tools.internal.xjc.reader.dtd.TDTDReader;
+
+/**
+ * @Author candy-wind
+ * @Data: 2020-05-29 10:30
+ * @Version 1.0
+ */
+public class ThreadLocalTest {
+
+
+//
+//
+//    //本地线程变量
+//    public static void main(String[] args) {
+//        ThreadLocal threadLocal = new ThreadLocal();
+//        threadLocal.set("1");
+//        threadLocal.remove();
+//        threadLocal.get();
+//    }
+
+}
