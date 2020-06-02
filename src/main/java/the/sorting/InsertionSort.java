b954863a6e418b2298dc70223fc0017ad1727f98
candy-wind <1314@qq.com>
2020-06-02 10:30:49 +0800

    插入排序

--- src/main/java/the/sorting/InsertionSort.java
+++ src/main/java/the/sorting/InsertionSort.java
@@ -22,12 +22,23 @@ public class InsertionSort {
     public static void main(String[] args) {
         int arr[] = {7, 5, 3, 2, 4};
         for (int i = 1; i < arr.length; i++) {
-
-
+            for (int j = i; j > 0; j--) {
+                //内存循环，与前面排好序的数据比较，如果后面的数据小于前面的则交换
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    //如果不小于，说明插入完毕，退出内层循环
+                    break;
+                }
+            }
 
         }
 
-
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
     }
 
---
+++ src/main/java/the/sorting/QuickSort.java
@@ -0,0 +1,19 @@
+package the.sorting;
+
+/**
+ * @Author candy-wind
+ * @Data: 2020-06-01 18:49
+ * @Version 1.0
+ *
+ *
+ *
+ *快速排序  ----
+ * 通过一趟排序将带排记录分割成独立的两部分，其中一部分记录的关键字均比另一部分关键字小，则分别对着两部分排序，达到整个序列有序
+ */
+public class QuickSort {
+
+
+
+
+
+}
