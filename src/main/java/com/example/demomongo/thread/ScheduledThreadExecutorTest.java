c0af60993fa61c93c810550a2942f4d44ec2d812
zhenghetian <zhenghetian@finupgroup.com>
2020-06-12 16:01:23 +0800

    修改线程

--- src/main/java/com/example/demomongo/thread/ScheduledThreadExecutorTest.java
+++ src/main/java/com/example/demomongo/thread/ScheduledThreadExecutorTest.java
@@ -14,15 +14,21 @@ public class ScheduledThreadExecutorTest {
 
         ScheduledThreadPoolExecutor exec =new ScheduledThreadPoolExecutor(1);
         //每隔一段时间就触发异常
-        exec.scheduleAtFixedRate(() -> { // TODO Auto-generated method stub //throw new RuntimeException();
-            System.out.println("===================");
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() { // TODO Auto-generated method stub //throw new RuntimeException();
+                // System.out.println("===================");
 
+            }
         }, 10000, 5000, TimeUnit.MILLISECONDS);
 
         //每隔一段时间打印系统时间，证明两者是互不影响的
-        exec.scheduleAtFixedRate(() -> { // TODO Auto-generated method stub
-            System.out.println(System.nanoTime());
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() { // TODO Auto-generated method stub
+                System.out.println(System.nanoTime());
 
+            }
         }, 10000, 2000, TimeUnit.MILLISECONDS);
 
 
