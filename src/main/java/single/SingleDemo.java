b2eee2244751255cc738b9ecc83baf4428e6f5a9
candy-wind <1314@qq.com>
2020-07-14 19:11:52 +0800

    复习了一下自己写的代码 今天学习了二叉树。。。哈哈哈哈

--- src/main/java/single/SingleDemo.java
+++ src/main/java/single/SingleDemo.java
@@ -5,20 +5,14 @@ package single;
  * @Data: 2020-04-27 18:48
  * @Version 1.0
  */
+
+//单例模式
 public class SingleDemo {
     //单例模式
 
 
 
     //一个类只有一个实例
-
-    public static void main(String[] args) {
-        String a="adfsa";
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("a  dfadfa");
-        System.out.println(stringBuffer.replace(1,2,"a"));
-    }
-
 
 
 }
